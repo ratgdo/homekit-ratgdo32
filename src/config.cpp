3730eb3aabf10d23f412e0b5b7d2ca5c31e7292e
David Kerr <david@kerrfamily.org>
2025-02-02 21:08:59 -0500

    Hook into ESP_LOGx() so we can route log messages from packages that use this to our logger service

--- src/config.cpp
+++ src/config.cpp
@@ -221,6 +221,7 @@ userSettings::userSettings()
         {cfg_laserEnabled, {false, false, false, helperLaser}},
         {cfg_laserHomeKit, {false, false, true, helperLaser}},
         {cfg_assistDuration, {false, false, 60, NULL}},
+        {cfg_espLogLevel, {false, false, ESP_LOG_ERROR, NULL}},
     };
 }
 
