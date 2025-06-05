3c85c89eef7ba621d611315e7a8a6cef5aedd618
Andrew Pearson <andrew@apearson.io>
2025-06-04 22:03:14 -0500

    Revert "Moving to at request time creating ipv6 addresses string"
    This reverts commit 5268ffdb2e98b6905d783479b0b1fde1e085e7bf.

--- src/homekit.cpp
+++ src/homekit.cpp
@@ -52,6 +52,9 @@ static bool rebooting = false;
 
 char qrPayload[21];
 
+// Buffer to hold all IPv6 addresses as a single string (max 8 addresses, ~40 chars each)
+char ipv6_addresses[320] = {0};
+
 /****************************************************************************
  * Callback functions, notify us of significant events
  */
@@ -106,15 +109,25 @@ void connectionCallback(int count)
     if (WiFi.dnsIP().type() == IPv4)
         userConfig->set(cfg_nameserverIP, WiFi.dnsIP().toString().c_str());
 
-    // IPv6
+    // IPv6 SLAAC
     esp_ip6_addr_t if_ip6[LWIP_IPV6_NUM_ADDRESSES];
     int nIPv6 = esp_netif_get_all_preferred_ip6(WiFi.STA.netif(), if_ip6);
     ESP_LOGI(TAG, "Found %d IPv6 addresses:", nIPv6);
-    for (int i = 0; i < nIPv6; i++) {
+
+    ipv6_addresses[0] = '\0'; // Clear the buffer
+    for (int i = 0; i < nIPv6; i++)
+    {
         String addrStr = IPAddress(IPv6, (const uint8_t *)if_ip6[i].addr, if_ip6[i].zone).toString();
 
         ESP_LOGI(TAG,"  %s", addrStr.c_str());
-     }
+
+        // Append to buffer, separated by comma if not first
+        if (i > 0) {
+            strlcat(ipv6_addresses, ",", sizeof(ipv6_addresses));
+        }
+
+        strlcat(ipv6_addresses, addrStr.c_str(), sizeof(ipv6_addresses));
+    }
 
     // With WiFi connected, we can now initialize the rest of our app.
     if (!softAPmode)
